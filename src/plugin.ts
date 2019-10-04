236eada526388027a0c1a32d4a21fd4ac866a24d
Daniel Roe <daniel@concision.co.uk>
2019-10-04 22:51:51 +0100

    style: fix eslint warnings

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,8 +1,9 @@
 import { InjectKey } from 'vue/types/options'
 import { Context } from '@nuxt/types'
 
+// eslint-disable-next-line
 const { createStore } = require('<%= options.buildDir %>/store')
-
+// eslint-disable-next-line
 const { getAccessorFromStore } = require('<%= options.libDir %>/utils')
 
 type Inject = (name: InjectKey, property: unknown) => void
