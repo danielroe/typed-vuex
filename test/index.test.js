988f9089497b6499de596c2091d66d6acc1dea9a
Daniel Roe <daniel@concision.co.uk>
2019-10-15 09:55:45 +0100

    test: test for presence of plugin in built files

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,21 @@
-// const path = require('path')
+const path = require('path')
+const fs = require('fs')
+
 const { Nuxt, Builder } = require('nuxt-edge')
+
+function walkDir(dir, callback) {
+  fs.readdirSync(dir).forEach(f => {
+    const dirPath = path.join(dir, f)
+    const isDirectory = fs.statSync(dirPath).isDirectory()
+    isDirectory ? walkDir(dirPath, callback) : callback(path.join(dir, f))
+  })
+}
+
+function iterateOnDirectory(directory, callback) {
+  walkDir(path.resolve(__dirname, directory), path =>
+    callback(path, fs.existsSync(path) && fs.readFileSync(path, 'utf8'))
+  )
+}
 
 describe('nuxt-typed-vuex', () => {
   let nuxt
@@ -14,17 +30,11 @@ describe('nuxt-typed-vuex', () => {
     await builder.build()
   }, 60000)
 
-  // test('build files (.nuxt)', async () => {
-  //   const buildFiles = klawSync(nuxt.options.buildDir).map(getRelativePath)
-
-  //   expect(buildFiles.filter(noJS)).toMatchSnapshot()
-  // })
-
-  // test('generate files (dist)', async () => {
-  //   const generateFiles = klawSync(nuxt.options.generate.dir).map(
-  //     getRelativePath
-  //   )
-
-  //   expect(generateFiles.filter(noJS)).toMatchSnapshot()
-  // })
+  test('build files (.nuxt) includes plugin', async () => {
+    const buildFiles = []
+    iterateOnDirectory(nuxt.options.buildDir, filename => {
+      buildFiles.push(path.relative(__dirname, filename))
+    })
+    expect(buildFiles.includes('fixture/.nuxt/nuxt-typed-vuex.js'))
+  })
 })
