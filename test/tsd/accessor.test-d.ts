6932d16307729674548f20281cff3ae1ae9bb7aa
Daniel Roe <daniel@concision.co.uk>
2019-11-03 16:30:50 +0000

    test: test accessor function typings

---
+++ test/tsd/accessor.test-d.ts
@@ -0,0 +1,49 @@
+import { DispatchOptions, CommitOptions } from 'vuex'
+import {expectType, expectError} from 'tsd'
+
+import  { getAccessorType  } from '../../lib/utils'
+import {
+  getters,
+  state,
+  actions,
+  mutations,
+} from '../fixture/store'
+
+import * as submodule from '../fixture/store/submodule'
+
+const pattern = {
+  getters,
+  state,
+  actions,
+  mutations,
+  modules: {
+    submodule: {
+      ...submodule,
+      namespaced: true,
+      modules: {
+        nestedSubmodule: {
+          ...submodule,
+          namespaced: true,
+        }
+      }
+    },
+  },
+}
+
+const accessorType = getAccessorType(pattern)
+const submoduleAccessorType = getAccessorType(submodule)
+
+expectType<string>(accessorType.fullEmail)
+interface CommitFunction {
+    (payload: string, options?: CommitOptions): void
+}
+expectType<CommitFunction>(accessorType.setEmail)
+
+interface ActionFunction {
+    (options?: DispatchOptions): Promise<void>
+}
+expectType<ActionFunction>(accessorType.resetEmail)
+
+expectType<string>(submoduleAccessorType.firstName)
+expectType<void>(submoduleAccessorType.initialise())
+expectError(submoduleAccessorType.initialise('argument'))
