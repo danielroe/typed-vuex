eaf81f42e00b0d4df71303289cb66664f3e28ffa
Daniel Roe <daniel@roe.dev>
2020-12-17 21:20:20 +0000

    test: specify corejs@3

--- packages/nuxt-typed-vuex/test/fixture/nuxt.config.js
+++ packages/nuxt-typed-vuex/test/fixture/nuxt.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   buildModules: ['@nuxt/typescript-build', '../../src'],
+  build: {
+    babel: {
+      presets(_ctx, [_preset, options]) {
+        options.corejs = 3
+      },
+    },
+  },
   typescript: {
-    typeCheck: false
-  }
+    typeCheck: false,
+  },
 }
